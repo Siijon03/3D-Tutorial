48844e991eaba9192a12a12ef4f263984945a733
Siijon03 <117677593+Siijon03@users.noreply.github.com>
2023-08-23 21:50:26 +0100

    Tutorial V0.0.2
    Fixed Movement, No Jumping needed.
    
    Next:
    Add Interactable Objects.

--- Assets/Basic_Movement.cs
+++ Assets/Basic_Movement.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Basic_Movement : MonoBehaviour
 {
     [Header("Movement")]
     //Freely able to change movement speed in the inspector.
     public float MovementSpeed;
+
+    //Decreases acceleration and momentuem, this allows us to move more naturally. 
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    //Uses the Player's Height
+    public float PlayerHeight;
+    //Uses Raycast to Check whether we are on the ground
+    public LayerMask GroundCheck;
+    //True or False condition when we are either on/off the ground. 
+    bool IsGrounded;
+
+    [Header("Speed Checker")]
+    public TMP_Text SpeedVarText;
 
     //Transforming the Orientation of an object.
     public Transform orientation;
@@ -30,7 +45,19 @@ public class Basic_Movement : MonoBehaviour
     //Will Constantly check for movement.
     private void Update()
     {
+        SpeedVarText.SetText("Speed: " + MovementSpeed);
+
         PlayerInput();
+        SpeedControl();
+
+        //Consistently Checking for the Ground.
+        IsGrounded = Physics.Raycast(transform.position + new Vector3(0,0.05f,0),Vector3.down, PlayerHeight * 0.5f + 0.3f, GroundCheck);
+
+        //Ground and Air Drag.
+        if (IsGrounded == true)
+            RB.drag = groundDrag;
+        else 
+            RB.drag = 0;
     }
 
     //Used to check physics
@@ -54,5 +81,18 @@ public class Basic_Movement : MonoBehaviour
 
         //Adds force to movement.
         RB.AddForce(10f * MovementSpeed * MovementDirection.normalized, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(RB.velocity.x, 0f, RB.velocity.z);
+
+        //limit velocity if needed.
+        if (flatVel.magnitude > MovementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * MovementSpeed;
+            //Controls and Limits Velocity on the X,Y,Z Axis
+            RB.velocity = new Vector3(limitedVel.x, RB.velocity.y, limitedVel.z);
+        }
     }
 }
